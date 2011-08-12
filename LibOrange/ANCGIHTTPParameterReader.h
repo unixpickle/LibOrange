25ef4507fd788edba35a4958465136cac804ce04
Chris Ballinger <chrisballinger@gmail.com>
2011-08-12 02:02:56 -0400

    fix iOS compilation error

--- LibOrange/ANCGIHTTPParameterReader.h
+++ LibOrange/ANCGIHTTPParameterReader.h
@@ -6,7 +6,12 @@
 //  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#ifdef TARGET_OS_IPHONE
+    #import <UIKit/UIKit.h>
+#else
+    #import <Cocoa/Cocoa.h>
+#endif
+
 #import "ANStringEscaper.h"
 
 @interface ANCGIHTTPParameterReader : NSObject {
